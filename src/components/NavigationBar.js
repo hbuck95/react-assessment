afaf67911d62f5c9b10e38b6ef3cf664e01f5a7e
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-02 11:16:46 +0100

    Created NavigationBar component

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem
+} from 'reactstrap';
+
+import { Link } from "react-router-dom";
+
+
+export default class NavigationBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div id="Navbar">
+                <Navbar color="dark" light expand="md">
+                    <NavbarBrand> <Link to="/" className="nlink">React Assessment</Link></NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <Link to="/register" className="nlink">Register</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/login" className="nlink">Login</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/viewUsers" className="nlink">View Users</Link>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
