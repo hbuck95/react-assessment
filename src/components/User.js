4cd851ab61602579dd26daa1e8327341b510a167
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-02 13:36:54 +0100

    Added user component

---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class User extends Component {
+
+    render() {
+        return (
+            <div id={this.props._id}>
+                <b>{this.props.username}</b>
+            </div>
+        )
+    };
+}
