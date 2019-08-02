1817e5c93db24e06cabc138c428c1e4c50c15912
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-02 14:19:21 +0100

    Tested UserList rendering

---
+++ src/__tests__/UserList.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
+
+import UserList from '../components/UserList';
+
+configure({adapter: new Adapter()});
+
+it('UserList renders', () => {
+    const compJson = renderer.create(<UserList/>).toJSON();
+    expect(compJson).toMatchSnapshot();
+  });
