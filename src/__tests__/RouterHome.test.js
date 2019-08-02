f89c31356845f2ed36b7f8fe71f50f752f301229
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-02 14:15:36 +0100

    Tested RouterHome rendering

---
+++ src/__tests__/RouterHome.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
+
+import RouterHome from '../components/RouterHome';
+
+configure({adapter: new Adapter()});
+
+it('RouterHome renders', () => {
+    const compJson = renderer.create(<RouterHome/>).toJSON();
+    expect(compJson).toMatchSnapshot();
+  });
